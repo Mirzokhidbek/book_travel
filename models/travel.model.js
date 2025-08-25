f03e85b9b3aa0dacd8a53bbd16633105b73de77c
Mirzokhidbek <mirodev@sju.ac.kr>
2025-08-25 13:38:06 +0900

    Initial commit

---
+++ models/travel.model.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+const travelModel = new Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  img: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = model("Trave", travelModel);
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const connectDB = require("./config/db");
+require("dotenv").config();
+
+//connect to database
+connectDB();
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server listening on Port: ${PORT}`));
